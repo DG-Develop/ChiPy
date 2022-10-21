f0b39f6646e1c034f183e38fb7a2f4f757ae93d4
David Gomez Pacheco <dgdband@gmail.com>
2022-10-21 16:00:13 -0500

    [ADD] Terminando de agregar el método que válida dos contratos el original y el escaneado y compara que el contenido sea iguales

--- flaskr/scripts/convertir_pdf_a_jpg.py
+++ flaskr/scripts/convertir_pdf_a_jpg.py
@@ -2,8 +2,9 @@ import os
 from pdf2image import convert_from_path
 import cv2
 import imutils
-from PIL import Image, ImageSequence, ImageFont, ImageDraw, ImageOps
+from PIL import Image, ImageFont, ImageDraw
 from pytesseract import pytesseract
+import numpy as np
 
 saving_folder = 'process'
 original_pages = convert_from_path(pdf_path='static/pdf/SAFIN-022-067.pdf', poppler_path='flaskr/scripts/poppler-22.04.0/Library/bin')
@@ -30,14 +31,49 @@ for i in range(numero_pagina_actual):
     
 
 
+def text_to_image(text):
+    flag = False
+    lista = list()
+    for linea in text.split('\n'):
+        if "No. SAFIN-022-067" in linea:
+            flag = True
+        
+        if flag:
+            lista.append(linea)
+        
+        
+    lineas = tuple(lista)
+    # lineas = tuple(linea.rstrip() for linea in text.split('\n'))
+    
+    font = ImageFont.truetype('static/fonts/QuatroSlab-Regular.ttf')
+    teststr = 'abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúÁÉÍÓÚ””'	
+    pt2px = lambda pt: int(round(pt * 96.0 / 72))
+    
+    # Alto y Ancho  imagen
+    max_width_line = max(lineas, key=lambda s: font.getbbox(s)[2])
+    max_width_font = pt2px(font.getbbox(max_width_line)[2])
+    max_height_font = pt2px(font.getbbox(teststr)[3])	
+    height = max_height_font * len(lineas)
+    width = int(round(max_width_font + 10))
+    
+    x = 10
+    y = 10	
+    image = Image.new('L', (690, 1000), color=255)
+    draw = ImageDraw.Draw(image)	
+	
+    for sline in lineas:
+        draw.text((x, y), sline, font=font)
+        y += max_height_font
+        
+    return image
+
 # pyteseract
 
 # No olvidar el .exe de tesseract para windows y en la ruta especificada
 path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     
 for i in range(numero_pagina_original):
-    
-    if i == 2:
+    if i == 1:
         break
     
     original = cv2.imread(f'process/original-page{i}.png')
@@ -61,63 +97,42 @@ for i in range(numero_pagina_original):
     
     # with open(os.path.join(saving_folder,f'texto-nuevo-page{i}.txt'),'w') as f: 
     #     f.write(str(text_nuevo))
-    
-    lines_original = tuple(linea.rstrip() for linea in text_original.split('\n'))
-    
-    # TODO: Obtener todos los caracteres diferentes dentro del contrato
-    
-    font = ImageFont.load_default()
-    teststr = 'abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúÁÉÍÓÚ””'	
-    pt2px = lambda pt: int(round(pt * 96.0 / 72))
-    
-    max_width_line = max(lines_original, key=lambda s: font.getsize(s)[0])
-    max_width_font = pt2px(font.getsize(max_width_line)[0])
-    max_height_font = pt2px(font.getsize(teststr)[1])	
-    height = max_height_font * len(lines_original)
-    width = int(round(max_width_font + 10))
-    
-    x = 10
-    y = 10	
-    image = Image.new('L', (width, height), color=255)
-    draw = ImageDraw.Draw(image)	
-	
-    for sline in lines_original:
-        draw.text((x, y), sline, font=font)
-        y += max_height_font
-    
-    image.save(f'texto-original-page{i}.png')
 
+    # OpenCV
+    imagen_original = text_to_image(text_original)
+    imagen_nuevo = text_to_image(text_nuevo)
+    
+    aray_original = np.array(imagen_original)
+    ocvim_original = cv2.cvtColor(aray_original, cv2.COLOR_RGB2BGR)
+    
+    aray_nuevo = np.array(imagen_nuevo)
+    ocvim_nuevo = cv2.cvtColor(aray_nuevo, cv2.COLOR_RGB2BGR)
+    # text_to_image(text_original, f'process/change-original-page{i}.png')
+    # text_to_image(text_nuevo, f'process/change-page{i}.png')
+    
+    # imagen_original = cv2.imread(f'process/change-original-page{i}.png')
+    # imagen_nuevo = cv2.imread(f'process/change-page{i}.png')
+    
+    diff = ocvim_original.copy()
+    cv2.absdiff(ocvim_original, ocvim_nuevo, diff)
+    
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
-# def txt2img(txtfile, tiffile):
-# 	with open(txtfile, encoding='windows-1255') as stxt:
-# 		slines = tuple(line.rstrip() for line in stxt.readlines())
+    for j in range(0, 3):
+        dilated =  cv2.dilate(gray.copy(), None, iterations=j+1)
+    
 
-# 	font = ImageFont.load_default()	
-# 	teststr = 'abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'	
-# 	pt2px = lambda pt: int(round(pt * 96.0 / 72))
-	
-# 	max_width_line = max(slines, key=lambda s: font.getsize(s)[0])
-# 	max_width_font = pt2px(font.getsize(max_width_line)[0])
-# 	max_height_font = pt2px(font.getsize(teststr)[1])	
-# 	height = max_height_font * len(slines)
-# 	width = int(round(max_width_font + 10))
-# 	linespacing = int(round(max_height_font * 1.0))
-# 	x = 10
-# 	y = 10	
-# 	image = Image.new('L', (width, height), color=255)
-# 	draw = ImageDraw.Draw(image)	
-	
-# 	for sline in slines:
-# 		draw.text((x, y), sline, font=font)
-# 		y += max_height_font
-		
-# 	image.save(tiffile)
-# 	image.show()	
-# 	return
+    (T, thresh) = cv2.threshold(dilated, 3, 255, cv2.THRESH_BINARY)
 
+    cnts = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
 
-# txt2img('txt2tif.py', 'test1.tiff')
-
+    for c in cnts:
+        (x,y,w,h) = cv2.boundingRect(c)
+        cv2.rectangle(ocvim_nuevo, (x,y), (x + w, y + h), (0, 255, 0), 2)
+        
+    cv2.imwrite(f'process/change-original-page{i}.png', ocvim_original)
+    cv2.imwrite(f'process/change-page{i}.png', ocvim_nuevo)
 
 
 
