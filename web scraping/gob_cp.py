aae76479b3bbb373b5f1892f1c536a5c1dd3c623
David Gomez Pacheco <dgdband@gmail.com>
2022-11-11 16:01:19 -0600

    [ADD] Agregando ajustes menores

--- web scraping/gob_cp.py
+++ web scraping/gob_cp.py
@@ -6,6 +6,7 @@ from zipfile import ZipFile
 # Web Scraping
 
 ## Get website content
+print('Get Website content')
 url = 'https://www.correosdemexico.gob.mx/SSLServicios/ConsultaCP/CodigoPostal_Exportar.aspx'
 content_data = requests.get(url, allow_redirects=True)
 soup = BeautifulSoup(content_data.content, 'html.parser')
@@ -14,6 +15,7 @@ view_state = soup.find(id='__VIEWSTATE')
 event_validation = soup.find(id='__EVENTVALIDATION')
 
 ## Generate de data to form request 
+print('Generate data to form request')
 payload = {
     '__EVENTTARGET': '',
     '__EVENTARGUMENT': '',
@@ -28,16 +30,20 @@ payload = {
 }
 
 ## Downloading postal codes from SEPOMEX
+print('Downloading postal codes from SEPOMEX')
 response = requests.post(url=url, data=payload)
 
 zip_path = 'static/excel/CPdescargaxls.zip'
 
 ## Writing zip
+print('Writing Zip')
 open(zip_path, 'wb').write(response.content)
 
 ## Extracting zip
+print('Extracting Zip')
 with ZipFile(zip_path, 'r') as zipObj:
     zipObj.extractall(path='static/excel')
 
 ## Remove zip
+print('Remove zip')
 remove(zip_path)
